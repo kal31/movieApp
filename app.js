7dbd6d92865e4b88afd546ffd849516798d67f39
Khalid Miah <khalid.miah@engineering.digital.dwp.gov.uk>
2022-04-18 18:36:39 +0100

    added now playing movies from api

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(helmet());
+// app.use(helmet());
 
 app.use('/', indexRouter);
 
